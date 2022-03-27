75af6b3a4a205e0280ce0e9b600aa62102e8d15a
pensive-fox <ksmat.rab@gmail.com>
2022-03-27 22:53:04 +0300

    init

---
+++ lesson-1--from-scratch/index.mjs
@@ -0,0 +1,31 @@
+import fs from 'fs'
+
+const names = ["Kolya", "Ksusha", "Vasilisa", "Oleg"];
+
+const table = names.map(
+    (name) => `<tr>
+      <td>Name: </td>
+      <td>${name}</td>
+  </tr>`
+  ).join("\n\t")
+
+const template = `<!DOCTYPE html>
+<html lang="ru">
+  <head>
+      <title>Lesson 1</title>
+  </head>
+  <body>
+    sdfdsf sdfsd 23423 eede2222 4sdsdfd 77777 7777 88888 99999  0000
+    <table border="3">
+        ${table}
+    </table>
+  </body>
+</html>
+`;
+
+
+const build = () => {
+  fs.writeFileSync('index.html', template);
+}
+
+build()
