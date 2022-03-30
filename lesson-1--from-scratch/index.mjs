583d8c2be3bdd7e52270a43332aea12db36b650c
pensive-fox <ksmat.rab@gmail.com>
2022-03-30 16:06:14 +0300

    Added names

--- lesson-1--from-scratch/index.mjs
+++ lesson-1--from-scratch/index.mjs
@@ -1,13 +1,13 @@
 import fs from 'fs'
 
 const names = ["Kolya", "Ksusha", "Vasilisa", "Oleg"];
-
+names.push("Galina","Sergei")
 const table = names.map(
-    (name) => `<tr>
-      <td>Name: </td>
-      <td>${name}</td>
-  </tr>`
-  ).join("\n\t")
+    (name) => `    <tr>
+          <td>Name: </td>
+          <td>${name}</td>
+        </tr>`
+  ).join("\n\t\t")
 
 const template = `<!DOCTYPE html>
 <html lang="ru">
@@ -17,7 +17,7 @@ const template = `<!DOCTYPE html>
   <body>
     sdfdsf sdfsd 23423 eede2222 4sdsdfd 77777 7777 88888 99999  0000
     <table border="3">
-        ${table}
+    ${table}
     </table>
   </body>
 </html>
